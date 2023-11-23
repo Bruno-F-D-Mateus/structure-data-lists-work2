6a833b8c991c45630bae3e4c8a179b68bafdd680
Bruno Mateus <brunofortunatomateus@gmail.com>
2023-11-23 17:43:57 +0100

    updates

--- main.c
+++ main.c
@@ -6,16 +6,15 @@ int main(){
     circular_list *list = init();
     list = insertNodeBegin(list, 'a');   
     list = insertNodeBegin(list, 'b');   
+   
+    /*
     list = insertNodeBegin(list, 'c');   
     list = insertNodeBegin(list, 'd'); 
     list = insertNodeBegin(list, 'a');   
-   
-    /*
 
     */
     printList(list);
-    printf("------------------\n");
-    divideList(list);
+    //divideList(list);
  
     removeList(list);
     return 0;
